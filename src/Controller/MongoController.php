a758ceb63ca928a3dd524e5dcdb756da0c99cba2
vagrant <vagrant@homestead>
2019-10-29 15:13:52 +0000

    initialization mongodb - user class/mongocontroller
     Committer: dcorroyer <dylan.corroyer@wanadoo.fr>

---
+++ src/Controller/MongoController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\ODM\MongoDB\MongoDBException;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Document\User;
+use Doctrine\ODM\MongoDB\DocumentManager as DocumentManager;
+
+class MongoController
+{
+    /**
+     * @Route("/mongoTest", methods={"GET"})
+     * @param DocumentManager $dm
+     * @return JsonResponse
+     * @throws MongoDBException
+     */
+    public function mongoTest(DocumentManager $dm)
+    {
+        $user = new User();
+        $user->setEmail("cocofe@cocofe.fr");
+        $user->setFirstname("cocofe");
+        $user->setLastname("cocofe");
+        $user->setPassword(md5("cocofe"));
+
+        $dm->persist($user);
+        $dm->flush();
+        return new JsonResponse(array('Status' => 'OK'));
+    }
+}
